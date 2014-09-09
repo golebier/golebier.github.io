bb5a1316b49ae705c1314a1b90ff57441741c14e
Gra <golebier@gmail.com>
2014-09-09 15:54:55 +0200

    Extended test view and controller, added.

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -7,17 +7,20 @@ app.config(function ($routeProvider) {
           templateUrl: 'src/html/views/main.html'
       })
       .when('/test', {
-    	  controller:  'main',
-          templateUrl: 'src/html/views/main.html'
+    	  controller:  'test',
+          templateUrl: 'src/html/views/test.html'
       })
       .otherwise({redirectTo: '/'});
 });
 
-app.controller('main', function($scope, $location, $route) {
-     $scope.data = [{ urlName: 'test', location_path: $location.path(),
-    	 location_url: $location.url(), route_current_templateUrl: $route.current.templateUrl,
-    	 route_current_params: $route.current.params}];
+app.controller('main', function($scope) {
      $scope.accounts = [{ name: "Linkedin", url: "http://pl.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35/"},
                         { name: "G+", url: "https://google.com/+RadoslawGolebiewski"},
                         { name: "local test", url: "#/test"}];
+});
+
+app.controller('test', function($scope, $location, $route) {
+     $scope.data = [{ urlName: 'test', location_path: $location.path(),
+    	 location_url: $location.url(), route_current_templateUrl: $route.current.templateUrl,
+    	 route_current_params: $route.current.params}];
 });
