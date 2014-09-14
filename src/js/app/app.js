6495282435695953fab727726163a79e944f3985
Gra <golebier@gmail.com>
2014-09-14 13:07:12 +0200

    Tabs changed to four spaces.

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -3,22 +3,22 @@ var app = angular.module('GhFp', ['ngRoute']);
 app.config(function ($routeProvider) {
    $routeProvider
       .when('/', {
-    	  controller:  'main',
+          controller:  'main',
           templateUrl: 'src/html/views/main.html'
       })
       .when('/test', {
-    	  controller:  function($scope, $location, $route) {
-			    		    $scope.urlName = 'test';
-			    			$scope.location_path = $location.path();
-			    			$scope.location_url = $location.url();
+          controller:  function($scope, $location, $route) {
+                            $scope.urlName = 'test';
+                            $scope.location_path = $location.path();
+                            $scope.location_url = $location.url();
 
-			    		    $scope.getName = function() {
-			    		   	 	return $scope.name;
-			    			};
-			    		    $scope.getNameWith = function(addThis) {
-			    		   	 	return $scope.name + addThis;
-			    			};
-    	  				},
+                            $scope.getName = function() {
+                                    return $scope.name;
+                            };
+                            $scope.getNameWith = function(addThis) {
+                                    return $scope.name + addThis;
+                            };
+                          },
           templateUrl: 'src/html/views/test.html'
       })
       .otherwise({redirectTo: '/'});
@@ -26,49 +26,49 @@ app.config(function ($routeProvider) {
 
 app.controller('main', function($scope) {
      $scope.accounts = [
-                        	{ name: "Linkedin", url: "http://pl.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35/"},
-                        	{ name: "G+", url: "https://google.com/+RadoslawGolebiewski"},
-                        	{ name: "local test", url: "#/test"}
-                    	];
+                            { name: "Linkedin", url: "http://pl.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35/"},
+                            { name: "G+", url: "https://google.com/+RadoslawGolebiewski"},
+                            { name: "local test", url: "#/test"}
+                        ];
 });
 
 app.controller('TextAreaWithLimitCtrl', function($scope) {
-	var MAX_LEN = 128;
-	var WARN_THRESHOLD = 16;
-	var WARN_THRESHOLD_CRITICAL = 8;
-	// TODO 3 steps colored info, by font or text background;
-	// TODO clear, add cleaning
-	// TODO then use clean just for tests
-	// TODO strike "Remaining" if more then MAX_LEN
-	$scope.remaining = function () {
-		return MAX_LEN - $scope.message.length;
-	};
+    var MAX_LEN = 128;
+    var WARN_THRESHOLD = 16;
+    var WARN_THRESHOLD_CRITICAL = 8;
+    // TODO 3 steps colored info, by font or text background;
+    // TODO clear, add cleaning
+    // TODO then use clean just for tests
+    // TODO strike "Remaining" if more then MAX_LEN
+    $scope.remaining = function () {
+        return MAX_LEN - $scope.message.length;
+    };
 
-	$scope.hasValidLength = function () {
-		return $scope.remaining() > 0;
-	};
+    $scope.hasValidLength = function () {
+        return $scope.remaining() > 0;
+    };
 
-	$scope.shouldWarn = function () {
-		return $scope.remaining() < WARN_THRESHOLD;
-	};
+    $scope.shouldWarn = function () {
+        return $scope.remaining() < WARN_THRESHOLD;
+    };
 
-	$scope.shouldWarnCritical = function () {
-		return $scope.remaining() < WARN_THRESHOLD_CRITICAL;
-	};
+    $scope.shouldWarnCritical = function () {
+        return $scope.remaining() < WARN_THRESHOLD_CRITICAL;
+    };
 });
 
 // TODO add more
 var NotificationsService = function () {
-	this.MAX_LEN = 10;
-	this.notificationsArchive = new NotificationsArchive();
-	this.notifications = [];
+    this.MAX_LEN = 10;
+    this.notificationsArchive = new NotificationsArchive();
+    this.notifications = [];
 };
 
 NotificationsService.prototype.push = function (notification) {
-	var newLen, notificationToArchive;
-	newLen = this.notifications.unshift(notification);
-	if (this.MAX_LEN < newLen) {
-		notificationToArchive = this.notifications.pop();
-		this.notificationsArchive.archive(notificationToArchive);
-	}
+    var newLen, notificationToArchive;
+    newLen = this.notifications.unshift(notification);
+    if (this.MAX_LEN < newLen) {
+        notificationToArchive = this.notifications.pop();
+        this.notificationsArchive.archive(notificationToArchive);
+    }
 };
--- src/test/js/app.js
+++ src/test/js/app.js
@@ -3,17 +3,17 @@ var app = angular.module('sp12', ['ngRoute']);
 
 app.controller('footer', function($scope) {
      $scope.accounts = [
-                        	{ name: "Linkedin",
-                        	   url: "http://pl.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35/",
+                            { name: "Linkedin",
+                               url: "http://pl.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35/",
                            iconUrl: "https://github.com/golebier/TemplateTeXCv/raw/master/Icons/linkedin.png"
                             },
-                        	{ name: "Google plus",
+                            { name: "Google plus",
                                url: "https://google.com/+RadoslawGolebiewski",
                            iconUrl: "https://github.com/golebier/TemplateTeXCv/raw/master/Icons/gplus.jpg"
                             },
-                        	{ name: "Github",
+                            { name: "Github",
                                url: "https://github.com/golebier",
                            iconUrl: "https://github.com/golebier/TemplateTeXCv/raw/master/Icons/github.png"
                             }
-                    	];
+                        ];
 });
