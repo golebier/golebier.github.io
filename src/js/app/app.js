1b433ac0e30163b4b853217bd3656b55b13ea71e
Gra <golebier@gmail.com>
2014-09-19 09:10:03 +0200

    Licensing informations per file added/changed.

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,3 +1,17 @@
+//
+//     Copyright (c) 1979, the Gra projects.
+//     Please see the AUTHORS file for details.
+//     All rights reserved.
+//     Use of this source code is governed by a MIT-style license
+//     that can be found in the LICENSE file.
+//
+//
+//     @author Gra <Gołębiewski Radosław A.>
+//             https://github.com/golebier or https://golebier.github.io
+//             https://google.com/+RadoslawGolebiewski
+//             http://www.linkedin.com/pub/rados%C5%82aw-go%C5%82%C4%99biewski/70/832/35
+//
+
 var app = angular.module('GhFp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
